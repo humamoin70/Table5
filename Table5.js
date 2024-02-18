072beef0fbf4fcd0333e1cc5280c65fb3839bf1c
humamoin70 <159626452+humamoin70@users.noreply.github.com>
2024-02-18 08:56:49 -0800

    Add files via upload

---
+++ Table5.js
@@ -0,0 +1,5 @@
+for (var i = 1; i <= 10; i++) {
+    var numb = 5;
+    var Answer = 5 * i;
+    console.log(numb + " X " + i + " = " + Answer);
+}
---
+++ Table5.ts
@@ -0,0 +1,6 @@
+for(let i = 1 ; i <= 10 ; i++){
+let numb = 5 ;
+let Answer = 5*i
+console.log(numb + " X " + i + " = " + Answer)
+
+}
